ace0a6ceaef26613cd5dcc9d749861e63bf8bbfe
Gianfranco Sullca <sullca.ortiz.gianfranco@gmail.com>
2023-01-07 12:23:08 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'actividad9';
+  num1: number =0;
+  num2: number =0;
+  resultado: number = 0;
+
+  selectValue: string = "";
+
+  calculate(){
+    console.log(this.selectValue)
+    switch (this.selectValue){
+      case "+":
+        this.resultado = this.num1 + this.num2;
+        break
+      case '-': 
+        this.resultado = this.num1 - this.num2;
+        break
+      case '/':
+        this.resultado = this.num1 / this.num2;
+        break
+      case '*': 
+        this.resultado = this.num1 * this.num2;
+        break
+    } 
+  }
+
+
+
 }
